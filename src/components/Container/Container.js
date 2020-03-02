835a09da16c28a617f78d8002f81b6b96a16a740
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2020-03-02 16:39:55 -0600

    added background image

---
+++ src/components/Container/Container.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Container = function() {
+    return (
+        <div className="background">
+
+        </div>
+    )
+}
+
+export default Container;
