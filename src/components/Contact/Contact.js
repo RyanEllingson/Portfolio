1c913ba171248bb44e2da69ac75fc8a620a410c6
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2020-03-06 15:34:12 -0600

    updated resume link address

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,7 +8,7 @@ const Contact = function() {
                 <p>My email address is <a href="mailto:ellin150@umn.edu">ellin150@umn.edu</a></p>
                 <p>My GitHub can be found at <a href="https://github.com/RyanEllingson">https://github.com/RyanEllingson</a></p>
                 <p>My LinkedIn can be found at <a href="https://linkedin.com/in/ryan-ellingson-25171614">https://linkedin.com/in/ryan-ellingson-25171614</a></p>
-                <p>Click <a href="https://drive.google.com/file/d/1AJm5sM0Ef2Bc1atgTOh2ETBvW61aJq_A/view?usp=sharing">here</a> to view my resume</p>
+                <p>Click <a href="https://drive.google.com/file/d/1bVKT2mr6_GKpdTUAUYgUfQIjXXPokX-4/view?usp=sharing">here</a> to view my resume</p>
             </div>
         </div>
     );
