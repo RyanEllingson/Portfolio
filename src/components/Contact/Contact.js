0e6d4b638f0da332ae33e7f32245c2a218e78de4
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2020-09-27 22:59:52 -0500

    added Maven Banking Application, and information about working with Java; updated resume link

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,7 +8,7 @@ const Contact = function() {
                 <p>My email address is <a href="mailto:ellin150@umn.edu">ellin150@umn.edu</a></p>
                 <p>My GitHub can be found at <a href="https://github.com/RyanEllingson">https://github.com/RyanEllingson</a></p>
                 <p>My LinkedIn can be found at <a href="https://linkedin.com/in/ryan-ellingson-25171614">https://linkedin.com/in/ryan-ellingson-25171614</a></p>
-                <p>Click <a href="https://drive.google.com/file/d/1q9xZ8V7Hz-nZ_cAUS4rgG9yIczx2zHMc/view?usp=sharing">here</a> to view my resume</p>
+                <p>Click <a href="https://drive.google.com/file/d/1UuoV2h5MxgEut-C0n0DOo5N0__d7uEzi/view?usp=sharing">here</a> to view my resume</p>
             </div>
         </div>
     );
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -10,7 +10,7 @@ const Profile = function() {
             </div>
             <div className="col-12 col-lg-9">
                 <p>My name is Ryan Ellingson.  I am a former chemical engineer who became a web developer because I enjoy solving technical problems and creating things with my own hands.</p>
-                <p>I am proficient in both front end and back end technologies.  I have built basic web pages using only HTML, CSS and JavaScript, as well as full stack applications using Node and Express back ends connecting to MySQL and MongoDB databases.  I have used Firebase both for database and for user authentication, and have also built front ends using React (which was used to make this portfolio!)</p>
+                <p>I am proficient in both front end and back end technologies.  On the front end side, I am comfortable working with vanilla HTML and JavaScript, but really enjoy working with React.js (which incidentally was used to create this portfolio).  I am also comfortable working with Bootstrap and custom CSS for styling.  On the back end, I can build and deploy fully-featured web applications in either Java, using Maven and a Tomcat web server, or in JavaScript using Node.js and Express.  I have created web projects in both languages that employ user authentication and database connectivity.  I have used MongoDB and Firebase databases, but I really enjoy working with SQL databases, and have worked with both MySQL and PostgreSQL database management systems.</p>
                 <p>Outside of work, I enjoy playing the bass and hiking.  I am also a bit of a gamer, and currently enjoy Magic: the Gathering and Final Fantasy XIV.</p>
             </div>
         </div>
