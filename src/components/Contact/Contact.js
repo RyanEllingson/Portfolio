0c1cf35084ff08d2ba859a3559ac7f76992779d3
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2021-01-17 11:04:02 -0600

    changed resume link to point to a file in the project rather than google drive

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./style.css";
+import Resume from "../../assets/RyanEllingsonResume.pdf";
 
 const Contact = function() {
     return (
@@ -8,7 +9,7 @@ const Contact = function() {
                 <p>My email address is <a href="mailto:ellin150@umn.edu">ellin150@umn.edu</a></p>
                 <p>My GitHub can be found at <a href="https://github.com/RyanEllingson">https://github.com/RyanEllingson</a></p>
                 <p>My LinkedIn can be found at <a href="https://linkedin.com/in/ryan-ellingson-25171614">https://linkedin.com/in/ryan-ellingson-25171614</a></p>
-                <p>Click <a href="https://drive.google.com/file/d/1UuoV2h5MxgEut-C0n0DOo5N0__d7uEzi/view?usp=sharing">here</a> to view my resume</p>
+                <p>Click <a href={Resume} target="_blank" rel="noopener noreferrer">here</a> to view my resume</p>
             </div>
         </div>
     );
